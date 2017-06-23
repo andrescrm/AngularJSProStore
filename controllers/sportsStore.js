1e083a2d4e6252d339dbe1aa576fbe838d8f3cc8
Andres Ramos <andramos@publicisgroupe.net>
2017-06-23 16:40:29 -0500

    Changed datasource to be consumed locally

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,5 +1,5 @@
 angular.module("sportsStore")
-.constant("dataUrl", "http://localhost:5500/products")
+.constant("dataUrl", "/datasources/data.json")
 .controller("sportsStoreCtrl", function($scope, $http, dataUrl){
     $scope.data = {};
 
